f3665c663970e82859e620cff3790c2c7d66f0ed
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-15 15:59:54 -0600

    Removes static root

--- DragMe_BE/settings.py
+++ DragMe_BE/settings.py
@@ -122,5 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
+# PROJECT_ROOT = os.path.disname(os.path)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
