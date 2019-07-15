9cc8577e364d2b4c5e1a4bbb4ae4bd1ec8cbb3bb
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-15 15:50:50 -0600

    update binaries

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jalenapenaligon/turing/4module/projects/DragMe_BE/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
