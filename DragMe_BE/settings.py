617a718a68730649ff97da01bd8f26fc7a6f8842
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-16 10:21:40 -0600

    Updates database config

--- DragMe_BE/settings.py
+++ DragMe_BE/settings.py
@@ -18,6 +18,9 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -78,9 +81,15 @@ WSGI_APPLICATION = 'DragMe_BE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
-
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'dragme',
+       'TEST': {
+            'NAME': 'dragme_test'
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
